3c05e5f1c36f46c1e7bb7a3bee21620946747c80
NagarjunMa <mallesh.n@northeastern.edu>
2024-04-10 09:14:09 -0400

    Initial Commit

---
+++ RecipeManagement/RecipeManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeManagementApp.swift
+//  RecipeManagement
+//
+//  Created by Nagarjun Mallesh on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
